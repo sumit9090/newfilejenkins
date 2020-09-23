7de111060b459e5a2bd494e2b7fe70490f4a0a90
sumit9090 <sumit.kalra121@gmail.com>
2020-09-23 11:19:53 +0530

    firstcommit

---
+++ src/test/java/basic_jenkins_demo/testcases/AnnotationsTest.java
@@ -0,0 +1,32 @@
+package basic_jenkins_demo.testcases;
+
+import org.testng.annotations.Test;
+
+
+
+
+
+
+public class AnnotationsTest {
+	
+	
+	
+	@Test
+	public void test1Test()
+	{
+		System.out.println("this is test1");
+	}
+	
+	@Test
+	public void test2Test()
+	{
+		System.out.println("this is test2");
+	}
+	
+	@Test
+	public void test3Test()
+	{
+		System.out.println("this is test3");
+	}
+
+}
